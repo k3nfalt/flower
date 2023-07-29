9b0a0175549e9ed5ecd0378431cc8a2e5fcf9b31
Alexander Tyurin <alexandertiurin@gmail.com>
2023-07-29 14:35:24 +0300

    Minor refactoring

--- baselines/dasha/dasha/client.py
+++ baselines/dasha/dasha/client.py
@@ -58,22 +58,19 @@ class CompressionClient(fl.client.NumPyClient):
         self._function.load_state_dict(state_dict, strict=True)
 
     def fit(self, parameters: NDArrays, config: Dict[str, Scalar]) -> Tuple[NDArrays, int, Dict]:
+        if config[self._SEND_FULL_GRADIENT]:
+            compressed_gradient = self._gradient_step(parameters)
+        else:
+            compressed_gradient = self._compression_step(parameters)
+        return compressed_gradient, len(self._targets), {self.SIZE_OF_COMPRESSED_VECTORS: self._compressor.num_nonzero_components()}
+    
+    def _calculate_gradient(self, parameters: NDArrays):
         self._set_parameters(parameters)
         self._function.zero_grad()
         loss = self._function(self._features, self._targets)
         loss.backward()
         gradients = self._get_current_gradients()
-        if config[self._SEND_FULL_GRADIENT]:
-            compressed_gradient = self._gradient_step(gradients)
-        else:
-            compressed_gradient = self._compression_step(gradients)
-        return compressed_gradient, len(self._targets), {self.SIZE_OF_COMPRESSED_VECTORS: self._compressor.num_nonzero_components()}
-    
-    def _get_momentum(self, dim):
-        if self._momentum is not None:
-            return self._momentum
-        self._momentum = 1 / (1 + 2 * self._compressor.omega())
-        return self._momentum
+        return gradients
 
     def evaluate(
         self, parameters: NDArrays, config: Dict[str, Scalar]) -> Tuple[float, int, Dict]:
@@ -97,38 +94,55 @@ class CompressionClient(fl.client.NumPyClient):
     def _get_current_gradients(self):
         return np.concatenate([val.grad.cpu().numpy().flatten() for val in self._function.parameters()])
         
-    def _gradient_step(self, gradients):
+    def _gradient_step(self):
         raise NotImplementedError()
     
-    def _compression_step(self, gradients):
+    def _compression_step(self):
         raise NotImplementedError()
 
 
 class DashaClient(CompressionClient):
-    def _gradient_step(self, gradients):
+    def _gradient_step(self, parameters: NDArrays):
+        gradients = self._calculate_gradient(parameters)
         self._gradient_estimator = gradients
         self._local_gradient_estimator = gradients
         compressed_gradient = IdentityUnbiasedCompressor().compress(self._gradient_estimator)
         return compressed_gradient
     
-    def _compression_step(self, gradients):
-        momentum = self._get_momentum(len(gradients))
+    def _compression_step(self, parameters: NDArrays):
+        gradients = self._calculate_gradient(parameters)
+        momentum = self._get_momentum()
         compressed_gradient = self._compressor.compress(
             gradients - self._local_gradient_estimator - momentum * (self._gradient_estimator - self._local_gradient_estimator))
         self._local_gradient_estimator = gradients
         self._gradient_estimator += decompress(compressed_gradient)
         return compressed_gradient
+    
+    def _get_momentum(self):
+        if self._momentum is not None:
+            return self._momentum
+        self._momentum = 1 / (1 + 2 * self._compressor.omega())
+        return self._momentum
 
 
 class MarinaClient(CompressionClient):
-    def _gradient_step(self, gradients):
+    def _gradient_step(self, parameters: NDArrays):
+        gradients = self._calculate_gradient(parameters)
         assert self._gradient_estimator is None
         self._local_gradient_estimator = gradients
         compressed_gradient = IdentityUnbiasedCompressor().compress(gradients)
         return compressed_gradient
     
-    def _compression_step(self, gradients):
+    def _compression_step(self, parameters: NDArrays):
+        gradients = self._calculate_gradient(parameters)
         assert self._gradient_estimator is None
         compressed_gradient = self._compressor.compress(gradients - self._local_gradient_estimator)
         self._local_gradient_estimator = gradients
         return compressed_gradient
+
+
+# class StochasticCompressionClient(fl.client.NumPyClient):
+
+
+# class DashaStochasticClient(fl.client.NumPyClient):
+    
